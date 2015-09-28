2f93b15606f09c40fcaac663fdb9527d2bccd109
Tim Foley <thetimfoley@gmail.com>
2015-09-28 16:52:14 -0400

    adding links and tidying up

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var links = ["http://www.npr.org/2015/09/20/439491238/first-listen-chvrches-every-open-eye", "http://www.npr.org/2015/09/16/436304229/first-listen-dungen-allas-sak", "http://www.npr.org/2015/09/02/436304494/first-listen-petite-noir-la-vie-est-belle-life-is-beautiful", "http://www.npr.org/2015/09/02/436304859/first-listen-gary-clark-jr-the-story-of-sonny-boy-slim", "http://www.npr.org/2015/09/16/439181153/first-listen-los-lobos-gates-of-gold", "http://www.npr.org/2015/09/16/439491704/first-listen-youth-lagoon-savage-hills-ballroom", "http://www.npr.org/2015/09/09/438655152/first-listen-windhand-griefs-infernal-flower", "http://www.npr.org/2015/09/09/438581988/first-listen-yo-yo-ma-kathryn-stott-songs-from-the-arc-of-life"];
+  var links = ["http://www.npr.org/2015/09/23/439514986/first-listen-wavves-v", "http://www.npr.org/2015/09/23/442274312/first-listen-autre-ne-veut-age-of-transparency", "http://www.npr.org/2015/09/20/439491238/first-listen-chvrches-every-open-eye", "http://www.npr.org/2015/09/16/436304229/first-listen-dungen-allas-sak", "http://www.npr.org/2015/09/02/436304494/first-listen-petite-noir-la-vie-est-belle-life-is-beautiful", "http://www.npr.org/2015/09/02/436304859/first-listen-gary-clark-jr-the-story-of-sonny-boy-slim", "http://www.npr.org/2015/09/16/439181153/first-listen-los-lobos-gates-of-gold", "http://www.npr.org/2015/09/16/439491704/first-listen-youth-lagoon-savage-hills-ballroom", "http://www.npr.org/2015/09/09/438655152/first-listen-windhand-griefs-infernal-flower", "http://www.npr.org/2015/09/09/438581988/first-listen-yo-yo-ma-kathryn-stott-songs-from-the-arc-of-life"];
   var context = {
     embedLinks: [],
     expiredLinks: []
@@ -22,13 +22,12 @@ $(document).ready(function() {
         } else {
           var title = $(result).find("article.story h1")[0].innerText;
           var archiveLink = $(result).find('#disqus-npr').attr('data-url');
-          console.log('archive: ' + archiveLink);
+          //console.log('archive: ' + archiveLink);
           var url = archiveLink;
 
           var entry = {title: title, url: url};
           context.expiredLinks.push(entry);
-          console.log(context.expiredLinks);
-          console.log(currentUrl);
+          //console.log(context.expiredLinks);
         }
 
 
